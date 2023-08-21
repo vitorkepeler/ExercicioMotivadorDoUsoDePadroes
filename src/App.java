3f68da884cb8bba0cabd891566d8f0f53c6ad98c
vkepeler <vitor.kepeler@e-core.com>
2023-08-21 20:10:10 -0300

    solicitação 1

--- src/App.java
+++ src/App.java
@@ -2,9 +2,11 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
+        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6,45);
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
+        System.out.println(esportivo);
 
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
@@ -14,5 +16,14 @@ public class App {
         basico.viaja(250);
         basico.viaja(150);
         System.out.println(basico);
+
+        System.out.println("\n\n----------------");
+        System.out.println("\nAbastencendo carro esportivo com gasolina");
+        esportivo.abastece(TipoCombustivel.GASOLINA, 55);
+        System.out.println(esportivo);
+        System.out.println("\nViajando com o carro esportivo");
+        esportivo.viaja(250);
+        esportivo.viaja(150);
+        System.out.println(esportivo);
     }
 }
