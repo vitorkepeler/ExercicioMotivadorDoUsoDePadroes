905278e0e6704e868eb17a09d10558e5be0d25e6
vkepeler <vitor.kepeler@e-core.com>
2023-08-21 20:18:53 -0300

    solicitação 2

--- src/App.java
+++ src/App.java
@@ -3,10 +3,12 @@ public class App {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6,45);
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5,70);
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
         System.out.println(esportivo);
+        System.out.println(utilitario);
 
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
@@ -25,5 +27,14 @@ public class App {
         esportivo.viaja(250);
         esportivo.viaja(150);
         System.out.println(esportivo);
+
+        System.out.println("\n\n----------------");
+        System.out.println("\nAbastencendo carro utilitario com gasolina");
+        utilitario.abastece(TipoCombustivel.DIESEL, 55);
+        System.out.println(utilitario);
+        System.out.println("\nViajando com o carro utilitario");
+        utilitario.viaja(250);
+        utilitario.viaja(150);
+        System.out.println(utilitario);
     }
 }
